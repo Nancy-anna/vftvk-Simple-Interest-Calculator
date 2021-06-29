b050b22f6011477ce89e9b6062e714ee484ccc5d
Nancy-anna <78694612+Nancy-anna@users.noreply.github.com>
2021-06-29 23:07:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 function compute()
 {
     p = document.getElementById("principal").value;
+    q = document.getElementById("rate").value;
+    r = document.getElementById("years").value;
     
 }
         
